5600ea7e02a900c8e99edc0536ab012664d876d9
Ebisuzawa Kurumi <ebisuzawakurumi@proton.me>
2024-10-29 01:10:01 +0800

    feat: init

--- App.tsx
+++ App.tsx
@@ -1,11 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { open, QueryResult } from "@op-engineering/op-sqlite";
+import { useEffect, useState } from "react";
+
+const db = open({ name: "test.db" });
+
+db.execute(
+  "CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY, name TEXT)",
+);
+
+function DatabaseComponent() {
+  const [result, setResult] = useState<any[] | null>(null);
+
+  useEffect(() => {
+    console.log("Effect mounted");
+
+    const unsubscribe = db.reactiveExecute({
+      query: "SELECT * FROM test",
+      arguments: [],
+      fireOn: [
+        {
+          table: "test",
+        },
+      ],
+      callback: (result: QueryResult) => {
+        console.log("Callback ran!");
+        setResult(result.rows._array);
+      },
+    });
+
+    // NOTE: unsubscribe() works when called here, but not when unmounting.
+    // unsubscribe();
+
+    return () => {
+      console.log("Effect unmounting");
+      // NOTE: "Callback ran!" still prints even though we've unsubscribed.
+      unsubscribe();
+    };
+  }, []);
+
+  return (
+    <View>
+      <Text>Database component</Text>
+      <Text>{result?.length}</Text>
+    </View>
+  );
+}
 
 export default function App() {
+  const [showDatabase, setShowDatabase] = useState(true);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button
+        title={showDatabase ? "Unmount Database" : "Mount Database"}
+        onPress={() => setShowDatabase(!showDatabase)}
+      />
+      {showDatabase && <DatabaseComponent />}
+      <Button
+        title="Execute query"
+        onPress={async () => {
+          console.log("Pressed!");
+          await db.transaction(async (tx) => {
+            tx.execute("INSERT INTO test (name) VALUES (?)", ["test"]);
+          });
+        }}
+      />
     </View>
   );
 }
@@ -13,8 +74,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ README.md
@@ -0,0 +1,25 @@
+# op-sqlite-useeffect-repro
+
+## Getting Started
+
+Install the dependencies:
+
+```bash
+npm install
+```
+
+Then, prebuild the native code:
+
+```bash
+npx expo prebuild
+```
+
+Then, run the app:
+
+```bash
+# Android
+npm run android
+
+# iOS
+npm run ios
+```
